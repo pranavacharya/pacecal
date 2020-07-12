ef3bae517cfcdcff047a372b80ec2eea85e50d84
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-13 01:00:48 +0530

    added budle for cjs umd and es6

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
-export default {
-  input: './index.js',
-  output: {
-    file: './build/pacecal.min.js',
-    format: 'umd',
-    name: 'pacecal',
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default [
+  {
+    input: './index.js',
+    output: {
+      name: 'pacecal',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [resolve(), commonjs()],
   },
-};
+  {
+    input: './index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+];
