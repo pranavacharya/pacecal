3876ef3dc2269a32f863b571280d2d681227fd90
Pranav Acharya <apranav.acharya@gmail.com>
2020-08-09 00:11:40 +0530

    added terser for minifying builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -10,13 +11,13 @@ export default [
       file: pkg.browser,
       format: 'umd'
     },
-    plugins: [resolve(), commonjs()]
+    plugins: [resolve(), commonjs(), terser()]
   },
   {
     input: './index.js',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', plugins: [terser()] },
+      { file: pkg.module, format: 'esm', plugins: [terser()] }
     ]
   }
 ];
